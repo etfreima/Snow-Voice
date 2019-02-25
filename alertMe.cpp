d564d46f922f81c2e3a7321c662b592ea9197750
etfreima <etfreima2080@gmail.com>
2019-02-24 23:27:05 -0500

    dev_v_a0.02

--- alertMe.cpp
+++ alertMe.cpp
@@ -1,11 +1,19 @@
 #include "pch.h"
 #include <stdio.h>
+#include <cstdio>
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <ctime>
+#include <iomanip>
+#include "libs/rapidjson/document.h"
+#include "libs/rapidjson/stringbuffer.h"
+#include "libs/rapidjson/writer.h"
+#include "libs/rapidjson/filereadstream.h"
 #include "../alertMe/curl/curl.h"
-#include <windows.data.json.h>
-//#include "firebase/app.h"
+#include <unordered_map>
+
+#pragma warning(disable:4996)
 
 #define CURL_STATICLIB
 
@@ -22,8 +30,6 @@ void delLink();
 
 int main()
 {
-
-
 	//Important variable declarations
 	std::string preURL = "https://api.darksky.net/forecast/";
 	std::string usrCoords;
@@ -33,7 +39,7 @@ int main()
 	std::ifstream file_("../alertMe/debugOld/infLog.txt");
 	if (file_.is_open()) {
 		while (getline(file_, line_)) {
-			std::cout << line_ << "\n";
+			//std::cout << line_ << "\n";
 		}
 		file_.close();
 	}
@@ -45,30 +51,66 @@ int main()
 
 	//Generates link with user coordinates
 	fullURL = preURL + line_ + "/" + usrCoords;
+
 	//Writes the link to a file
 	std::cout << writeLink(fullURL);
+
 	//New line
 	std::cout << std::endl;
 
 
-	//********************************************
-	//********************cURL********************
+			//********************************************
+			//********************cURL********************
 
-	CURL *curl;
-	CURLcode res;
-	struct curl_slist *headers = NULL; //init to NULL is important
-	//headers = curl_slist_append(headers, "Accept: application/json");
-	//curl_slist_append(headers, "Content-Type: application/json");
-	//curl_slist_append(headers, "charsets: utf-8");
+			CURL *curl;
+			CURLcode res;
+			struct curl_slist *headers = NULL; //init to NULL is important
+			//headers = curl_slist_append(headers, "Accept: application/json");
+			//curl_slist_append(headers, "Content-Type: application/json");
+			//curl_slist_append(headers, "charsets: utf-8");
 
-	//curl = curl_easy_init();
+			//curl = curl_easy_init();
 
+			//********************cURL********************
+			//********************************************
+
+
+			//********************************************
+			//****************JSONCPP*********************
+			
+			//std::ifstream jFile("../alertMe/debugOld/repoOld.json");
+			FILE* fp = fopen("../alertMe/debugOld/repoOld.json", "rb");
+
+			char readBuffer[65536];//in bytes
+
+			rapidjson::FileReadStream is(fp, readBuffer, sizeof(readBuffer));
+
+			rapidjson::Document d;
+			d.ParseStream(is);
+			
+			const rapidjson::Value& prog = d["data"];
+
+			//Loops through each element of data array
+			for (rapidjson::Value::ConstValueIterator p = prog.Begin(); p != prog.End(); p++) {
+				std::time_t unixTime = (*p)["time"].GetInt();
+
+				//Output formatted forecast
+				std::cout << std::put_time(std::gmtime(&unixTime), "%c:") << " " 
+						  << (*p)["summary"].GetString() << " " << (*p)["temperature"].GetFloat()
+						  << "C " << std::endl;
+				
+			}
+
+			fclose(fp);
+			//****************JSONCPP*********************
+			//********************************************
 
 	//Waits for input and deletes file with link
 	//delLink();
+
 }//Main function
 
-
+//Function to get user coordinates and verify the validity of them
 std::string getCoords() {
 	bool t = false;
 
@@ -96,6 +138,8 @@ std::string getCoords() {
 
 //Function to write fullLink to a file
 std::string writeLink(std::string fullLink) {
+
+	//Future: get current working directory or host files on server
 	std::string pchPath = "../alertMe/debugOld/pchDep.txt";
 
 	std::ofstream pchDep(pchPath);
@@ -107,6 +151,7 @@ std::string writeLink(std::string fullLink) {
 	return fullLink;
 }
 
+//Function to delete created file
 void delLink() {
 	system("pause");
 	std::remove("debugOld/pchDep.txt");
